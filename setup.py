b8045d2adf9cf42ec37aacce95407400e022b148
Ryusei Abe <s21c1007qn@s.chibakoudai.jp>
2026-02-05 11:50:46 +0900

    実行名をsysinfo用に整理（talker/listenerを改名）

--- setup.py
+++ setup.py
@@ -2,29 +2,30 @@ import os
 from glob import glob
 from setuptools import setup
 
-package_name = 'mypkg'
+package_name = 'sysinfo_pub'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
-        (os.path.join('share', package_name), glob('launch/*.launch.py')),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='abe',
     maintainer_email='s21c1007qn@s.chibakoudai.jp',
-    description='a package for praction',
+    description='Publish system information as ROS 2 messages.',
     license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = mypkg.talker:main',
-            'listener = mypkg.listener:main',
+            'sysinfo_pub = sysinfo_pub.sysinfo_pub:main',
+            'sysinfo_sub = sysinfo_pub.sysinfo_sub:main',
         ],
     },
 )
+
